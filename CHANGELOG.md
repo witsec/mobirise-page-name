271c75901a3a5eb593f25ee430ae89d4617688fa
Mark <mark@witsec.nl>
2018-11-12 20:28:28 +0100

    Added additional page settings and 'learn more' link in extension description

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,11 @@
 
 All notable changes to this project will be documented in this file.
 
+## 0.2 (2018-11-xx)
+
+- Added additional page settings
+- Added 'Learn more...' link in extension description
+
 ## 0.1 (2018-11-06)
 
-### Summary
 This is the initial release of the Page Name extension for Mobirise.
--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@
                 var a = this;
                 a.addFilter("pageNameFullEdit", function() {
                     return !0
-                })
+                });
+                a.$body.find("#app-page-settings .app-layer-cont").append('<div class="form-group"><label for="page_header_custom">Inside &lt;head&gt; code:</label><textarea id="page_header_custom" class="form-control" data-page-settings="header_custom" rows="5" placeholder="Paste any valid HTML code here. The code will be inserted to the end of &lt;head&gt section, right before &lt;/head&gt"></textarea></div><div class="form-group"><label for="page_footer_custom">End of &lt;body&gt; code:</label><textarea id="page_footer_custom" class="form-control" data-page-settings="footer_custom" rows="5" placeholder="Paste any valid HTML code here. The code will be inserted to the end of <body> section, right before &lt;/body&gt; tag"></textarea></div><div class="form-group"><label for="page_before_html">Before &lt;!DOCTYPE&gt;, &lt;html&gt; and &lt;head&gt; tags:</label><textarea id="page_before_html" class="form-control" data-page-settings="html_before" rows="5" placeholder="Paste the code you want to have in the VERY FIRST LINE of your page, before &lt;!DOCTYPE&gt;, <html> and <head> tags. Use for server side scripts (PHP, ASP, etc)"></textarea></div>');
             }
         }
     })
